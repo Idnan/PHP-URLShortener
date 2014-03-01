ff6181f2cd6be14d7f017fa77c07b3e987d0f569
kamranahmed.se <kamranahmed.se@gmail.com>
2014-03-01 10:27:03 +0500

    Validate through Javascript and PHP shortening

--- lib/Application.php
+++ lib/Application.php
@@ -8,16 +8,95 @@ class Application extends Database
 {
 	public function __construct()
 	{
+		parent::__construct();
+
 		if ( isset($_POST['url']) && !empty($_POST['url']) )  {
-			$this->shortenUrl( $_POST['url'] );
+			$this->processUrl( $_POST['url'] );
 		} else if (isset($_GET['shortened_code'])) {
 			$this->handleRedirection( $_GET['shortened_code'] );
 		}
 	}
 
-	public function shortenUrl( $url )
+	public function validateUrl( $url )
 	{
+		if(empty($url)) {
+			return ERROR_NO_URL;
+		}
+		else if(filter_var($url, FILTER_VALIDATE_URL) === false) {
+			return ERROR_INVALID_URL;
+		}
+		else if( $this->alreadyShortened( $url ) )
+		{	
+			return ERROR_IS_SHORTENED;
+		} else { 
+			return true;
+		}
+	}
+
+	public function alreadyShortened( $url )
+	{
+		if ( URL_CHUNK ) {
+			return (preg_match("/" . URL_CHUNK . "/i",$url)) ? true: false;
+		}
+	}
+
+	public function generate_code()
+	{
+		$str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		return substr(str_shuffle($str),0,6);
+	}
+
+	public function is_code_unique( $code )
+	{
+		$sql = 'SELECT COUNT(url_id) AS url_count FROM urls WHERE code=:code LIMIT 1';
 		
+		$query = $this->db->prepare( $sql );
+		$query->execute( array( ':code' => $code ));
+
+		$result = $query->fetch( PDO::FETCH_ASSOC );
+		return ( $result['url_count'] == 0 ) ? true : false;
+	}
+
+	public function shortenUrl( $url, $code )
+	{
+		$sql = 'INSERT INTO urls( url, code ) VALUES( :url, :code)';
+
+		$query = $this->db->prepare( $sql );
+		$query->execute( array( ':url' => $url, ':code' => $code) );
+
+		return ( $query->rowCount() != 1 ) ? ERROR_UNKNOWN : SHORTENED_SUCCESS;
+	}
+
+	public function processUrl( $url )
+	{
+		$isValid = $this->validateUrl( $url );
+
+		if ( $isValid === true ) {
+
+			while(true)
+			{
+				$code = $this->generate_code();
+				if( $this->is_code_unique($code) )
+				{
+					$result = $this->shortenUrl( $url, $code );
+
+					if ( $result === true ) {
+						echo URL . $code;
+						break 1;
+					} else {
+						echo $result;
+						break 1;
+					}
+				}
+			}
+
+		} else {
+			
+			$error[] = $isValid;
+
+			$json = json_encode( $error );
+			echo $json;
+		}
 	}
 
 	public function handleRedirection( $code )
--- lib/Database.php
+++ lib/Database.php
@@ -5,5 +5,16 @@
 */
 class Database
 {
-	
+	public $db = null;
+
+	public function __construct()
+	{
+		$this->openConnection();
+	}	
+
+	public function openConnection()
+	{
+		$options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
+        $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $options);
+	}
 }
